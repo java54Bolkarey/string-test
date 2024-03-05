f8955193ee9da449ba6dd18f743c2748e3d3b8eb
Bolkarey <lbolkarey@mail.ru>
2024-03-05 22:29:55 +0200

    hw

--- src/telran/string/test/StringTest.java
+++ src/telran/string/test/StringTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 import  org.junit.jupiter.api.DisplayName;
 class StringTest {
 
